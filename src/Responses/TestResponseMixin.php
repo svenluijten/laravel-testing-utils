df9b7b8327f54fb2cf28f61e402f8a35a67ecec5
Hakan Aktas <iletisim@hakanaktas.com.tr>
2019-03-09 01:09:14 +0300

    added assertViewHasDeep assertion to TestResponse

---
+++ src/Responses/TestResponseMixin.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Sven\LaravelTestingUtils\Responses;
+
+use PHPUnit\Framework\Assert as PHPUnit;
+use Illuminate\Support\Arr;
+
+class TestResponseMixin
+{
+    /**
+     * Assert that a key exists in the response view's data.
+     *
+     * @return \Closure
+     */
+    public function assertViewHasDeep(): callable
+    {
+        return function ($key, $value = null) {
+            $this->ensureResponseHasView();
+
+            $data = $this->original->getData();
+            $keys = explode('.', $key);
+
+            foreach ($keys as $key) {
+                if (Arr::accessible($data)) {
+                    PHPUnit::assertTrue(Arr::exists($data, $key));
+                } elseif (is_object($data)) {
+                    PHPUnit::assertTrue(isset($data->{$key}));
+                } else {
+                    PHPUnit::fail('Data is not an object or array.');
+                }
+
+                $data = data_get($data, $key);
+            }
+
+            if ($value instanceof Closure) {
+                PHPUnit::assertTrue($value($data));
+            } elseif ($value instanceof Model) {
+                PHPUnit::assertTrue($value->is($data));
+            } else {
+                PHPUnit::assertEquals($value, $data);
+            }
+
+            return $this;
+        };
+    }
+}
---
+++ tests/Responses/SampleModel.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Sven\LaravelTestingUtils\Tests\Responses;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SampleModel extends Model {
+    //
+}
